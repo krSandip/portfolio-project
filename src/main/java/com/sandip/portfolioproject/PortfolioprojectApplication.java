14811270ab8f73caed8ad3295d0860c3209645e9
happyraina <sink10704@gmail.com>
2025-09-03 12:05:23 +0530

    initial commit

---
+++ src/main/java/com/sandip/portfolioproject/PortfolioprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sandip.portfolioproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioprojectApplication.class, args);
+	}
+
+}
